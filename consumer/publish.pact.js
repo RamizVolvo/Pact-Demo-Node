1ce8dbc3837301fe06b72625158c53c4d5f518ed
Sheikh Ramizuddin <rsheikh@volvocars.com>
2021-11-24 12:06:39 +0100

    initial commit

---
+++ consumer/publish.pact.js
@@ -0,0 +1,38 @@
+const pact = require('@pact-foundation/pact-node');
+const path = require('path');
+require('dotenv').config()
+if (!process.env.CI && !process.env.PUBLISH_PACT) {
+    console.log("skipping Pact publish...");
+    return
+}
+
+let pactBrokerUrl = process.env.PACT_BROKER_URL || 'http://localhost:80';
+let pactBrokerUsername = process.env.PACT_BROKER_USERNAME || 'pactbroker';
+let pactBrokerPassword = process.env.PACT_BROKER_PASSWORD || 'pactbroker';
+
+// const gitHash = require('child_process')
+//     .execSync('git rev-parse --short HEAD')
+//     .toString().trim();
+
+const opts = {
+    pactFilesOrDirs: [path.resolve(__dirname, './pacts/')],
+    pactBroker: pactBrokerUrl,
+    pactBrokerUsername: pactBrokerUsername,
+    pactBrokerPassword: pactBrokerPassword,
+    tags: ['prod', 'test'],
+    consumerVersion: "1.0"
+};
+
+pact
+    .publishPacts(opts)
+    .then(() => {
+        console.log('Pact contract publishing complete!');
+        console.log('');
+        console.log(`Head over to ${pactBrokerUrl} and login with`);
+        console.log(`=> Username: ${pactBrokerUsername}`);
+        console.log(`=> Password: ${pactBrokerPassword}`);
+        console.log('to see your published contracts.')
+    })
+    .catch(e => {
+        console.log('Pact contract publishing failed: ', e)
+    });
---
+++ provider/product/product.routes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const controller = require('./product.controller');
+
+router.get("/products/:id", controller.getById);
+router.get("/products", controller.getAll);
+
+module.exports = router;
