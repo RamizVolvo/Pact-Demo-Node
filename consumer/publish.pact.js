f4c757442433baa43827401698662fee1982ddc1
Ramiz <RSHEIKH@volvocars.com>
2021-12-02 16:35:37 +0100

    Integrate with pactflow

--- consumer/publish.pact.js
+++ consumer/publish.pact.js
@@ -1,38 +1,43 @@
-const pact = require('@pact-foundation/pact-node');
-const path = require('path');
-require('dotenv').config()
+const pact = require("@pact-foundation/pact-node");
+const path = require("path");
+require("dotenv").config();
 if (!process.env.CI && !process.env.PUBLISH_PACT) {
-    console.log("skipping Pact publish...");
-    return
+  console.log("skipping Pact publish...");
+  return;
 }
 
-let pactBrokerUrl = process.env.PACT_BROKER_URL || 'http://localhost:80';
-let pactBrokerUsername = process.env.PACT_BROKER_USERNAME || 'pactbroker';
-let pactBrokerPassword = process.env.PACT_BROKER_PASSWORD || 'pactbroker';
+let pactBrokerUrl = process.env.PACT_BROKER_URL || "http://localhost:80";
+let pactBrokerUsername = process.env.PACT_BROKER_USERNAME || "pactbroker";
+let pactBrokerPassword = process.env.PACT_BROKER_PASSWORD || "pactbroker";
+
+let pactBrokerBaseUrl = process.env.PACT_BROKER_BASE_URL;
+let pactBrokerToken = process.env.PACT_BROKER_TOKEN;
 
 // const gitHash = require('child_process')
 //     .execSync('git rev-parse --short HEAD')
 //     .toString().trim();
 
 const opts = {
-    pactFilesOrDirs: [path.resolve(__dirname, './pacts/')],
-    pactBroker: pactBrokerUrl,
-    pactBrokerUsername: pactBrokerUsername,
-    pactBrokerPassword: pactBrokerPassword,
-    tags: ['prod', 'test'],
-    consumerVersion: "1.0"
+  pactFilesOrDirs: [path.resolve(__dirname, "./pacts/")],
+  //pactBroker: pactBrokerUrl,
+  //pactBrokerUsername: pactBrokerUsername,
+  //pactBrokerPassword: pactBrokerPassword,
+  pactBroker: pactBrokerBaseUrl,
+  pactBrokerToken: pactBrokerToken,
+  tags: ["prod", "test",process.env.GIT],
+  consumerVersion: "1.0",
 };
 
 pact
-    .publishPacts(opts)
-    .then(() => {
-        console.log('Pact contract publishing complete!');
-        console.log('');
-        console.log(`Head over to ${pactBrokerUrl} and login with`);
-        console.log(`=> Username: ${pactBrokerUsername}`);
-        console.log(`=> Password: ${pactBrokerPassword}`);
-        console.log('to see your published contracts.')
-    })
-    .catch(e => {
-        console.log('Pact contract publishing failed: ', e)
-    });
+  .publishPacts(opts)
+  .then(() => {
+    console.log("Pact contract publishing complete!");
+    console.log("");
+    console.log(`Head over to ${pactBrokerUrl} and login with`);
+    console.log(`=> Username: ${pactBrokerUsername}`);
+    console.log(`=> Password: ${pactBrokerPassword}`);
+    console.log("to see your published contracts.");
+  })
+  .catch((e) => {
+    console.log("Pact contract publishing failed: ", e);
+  });
